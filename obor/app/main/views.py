8bee7cd842efbd22284bce930b832fd76fa8e040
roseou <598959770@qq.com>
2017-09-10 13:21:17 +0800

    edit

--- obor/app/main/views.py
+++ obor/app/main/views.py
@@ -4,7 +4,24 @@ from flask import render_template
 
 
 # test views
-@main.route('/test/')
-def test():
-    return "<h1>just tell you everything is ok!</h1>"
+@main.route('/')
+def home():
+    return render_template('home.html')
+
+@main.route('/about/')
+def about():
+    return render_template('about.html')
+
+@main.route('/learn/')
+def learn():
+    return render_template('learn.html')
+
+@main.route('/login/')
+def login():
+    return render_template('login.html')
+
+@main.route('/manage/')
+def manage():
+    return render_template('manage.html')
+
 

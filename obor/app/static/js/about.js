4674911004d227981603e2b4316c1408c92280bf
Elegenthus <996886231@qq.com>
2017-09-10 18:44:14 +0800

    change path

--- obor/app/static/js/about.js
+++ obor/app/static/js/about.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "942967918a1fb08b79dd"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "7a54271221a4e841386c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -570,7 +570,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/static/";
 
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
